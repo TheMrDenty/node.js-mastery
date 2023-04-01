78f1242bc46150d060f0196cbcf9a9d59dc50477
Jamie Taber-Plaisted <jamietp113@gmail.com>
2023-04-01 12:34:21 -0400

    set up routing for CRUD operations

--- task-manager/routes/tasks.js
+++ task-manager/routes/tasks.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 // calls getAllTasks function from tasks controller
-const {getAllTasks} = require('../controllers/tasks')
+const {getAllTasks, createTask, getTask, updateTask, deleteTask} = require('../controllers/tasks')
 
-router.route('/').get(getAllTasks);
+router.route('/').get(getAllTasks).post(createTask);
+router.route('/:id').get(getTask).patch(updateTask).delete(deleteTask);
 
 
 module.exports = router;
