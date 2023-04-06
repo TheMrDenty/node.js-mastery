219acf2e70fb6ea681fa4118624365189f76fbe3
Jamie Taber-Plaisted <jamietp113@gmail.com>
2023-04-06 16:03:54 -0400

    refactored getAll controller with asyncWrapper middleware to simplyify code in the controllers

--- task-manager/controllers/tasks.js
+++ task-manager/controllers/tasks.js
@@ -1,15 +1,15 @@
 const Task = require('../models/Task')
+const asyncWrapper = require('../middleware/async');
 
-const getAllTasks = async (req, res) => {
-    try {
-        const tasks = await Task.find({});
-        res.status(200).json({tasks});
-        /* res.status(200).json({tasks, amount: tasks.length }); */
-        /* res.status(200).json({ success:true, data:{tasks, amount: tasks.length} }); */
-    } catch (error) {
-        res.status(500).json({msg:error})
-    }
-}
+
+
+const getAllTasks = asyncWrapper(  async (req, res) => {
+    
+    const tasks = await Task.find({});
+    res.status(200).json({tasks});
+    /* res.status(200).json({tasks, amount: tasks.length }); */
+    /* res.status(200).json({ success:true, data:{tasks, amount: tasks.length} }); */
+})
 
 const createTask = async (req,res) => {
     try {
