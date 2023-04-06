b897752945f322d5fd60d7d482437b34c68adb85
Jamie Taber-Plaisted <jamietp113@gmail.com>
2023-04-06 15:54:14 -0400

    added not-found middleware for routes that arent found in the code

--- task-manager/app.js
+++ task-manager/app.js
@@ -8,11 +8,11 @@ const tasks = require('./routes/tasks');
 const connectDB = require('./db/connect');
 // brings in env variables
 require('dotenv').config();
-
+const notFound = require('./middleware/not-found');
 
 // middleware
-app.use(express.static('./public'))
-app.use(express.json())
+app.use(express.static('./public'));
+app.use(express.json());
 
 
 // routes
@@ -20,11 +20,7 @@ app.use(express.json())
 
 app.use('/api/v1/tasks', tasks);
 
-// app.get('/api/v1/tasks')         - get all the tasks
-// app.post('/api/v1/tasks')        - create a new task
-// app.get('/api/v1/tasks/:id')     - get single task
-// app.patch('/api/v1/tasks/:id')   - update task
-// app.delete('/api/v1/tasks/:id')  - delete task
+app.use(notFound);
 
 const port = 3000;
 
