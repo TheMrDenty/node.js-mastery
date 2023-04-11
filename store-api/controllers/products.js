fda1cc53b6ed0b377354356ed319805a2af7f25a
Jamie Taber-Plaisted <jamietp113@gmail.com>
2023-04-11 15:32:56 -0400

    added select, skip, and limit filters

--- store-api/controllers/products.js
+++ store-api/controllers/products.js
@@ -1,12 +1,14 @@
 const Product = require('../models/product');
 
 const getAllProductsStatic = async (req,res) => {
-    const products = await Product.find({}).sort('-name price');
+    const products = await Product.find({})
+        .sort('name')
+        .select('name price')
     res.status(200).json({products, nbHits: products.length});
 }
 
 const getAllProducts = async (req,res) => {
-    const {featured, company, name, sort} = req.query;
+    const {featured, company, name, sort, fields } = req.query;
     const queryObject = {};
 
     if(featured){
@@ -20,12 +22,33 @@ const getAllProducts = async (req,res) => {
     }
     /* console.log(queryObject); */
     let result = Product.find(queryObject);
+    // sort
     if(sort){
         const sortList = sort.split(',').join(' ');
         result = result.sort(sortList);
     } else {
         result = result.sort('createdAt');
     }
+
+    // fields
+    if (fields) {
+        const fieldsList = fields.split(',').join(' ');
+        result = result.select(fieldsList);
+    }
+
+    // pagination
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const skip = (page-1) * limit;
+
+    result = result.skip(skip).limit(limit);
+
+    // 23 products
+    // divided by 4 = 7 7 7 2 per page
+    // multiplied by limit
+
+
+
     const products = await result;
     res.status(200).json({ products, nbHits: products.length})
 }
