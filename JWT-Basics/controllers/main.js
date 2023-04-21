4d53f21dc6f361431c1366591b25601aec9f7d30
Jamie Taber-Plaisted <jamietp113@gmail.com>
2023-04-21 17:50:36 -0400

    setup JWT token

--- JWT-Basics/controllers/main.js
+++ JWT-Basics/controllers/main.js
@@ -4,6 +4,7 @@
 
 // setup authentication so only the request with JWT can access the dashboard
 
+const jwt =require('jsonwebtoken');
 const CustomAPIError = require('../errors/custom-error')
 
 const login = async (req,res) => {
@@ -17,8 +18,15 @@ const login = async (req,res) => {
         throw new CustomAPIError('Please provide email and password', 400)
     }
 
+    // just for demo, normally provided by DB!
+    const id = new Date().getDate();
+
+    // try to keep payload small, better experience for the user
+    // the simple environment variable is just for demo, in production use long, complex, and unguessable string value
+    const token = jwt.sign({id, username}, process.env.JWT_SECRET,{expiresIn:'30d'})
+
   
-    res.send('Fake Login/Register/Signup Route');
+    res.status(200).json({msg:'user created', token})
 }
 
 const dashboard = async (req,res) => {
