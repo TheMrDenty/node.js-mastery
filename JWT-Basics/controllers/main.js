47b8e35d74b90518b7ead095b8e52c6d906bc647
Jamie Taber-Plaisted <jamietp113@gmail.com>
2023-04-21 17:35:18 -0400

    setup error handling

--- JWT-Basics/controllers/main.js
+++ JWT-Basics/controllers/main.js
@@ -1,7 +1,24 @@
+// check username, password in post(login) request
+// if it exists, create ew JWT
+// send back-end to front-end
 
+// setup authentication so only the request with JWT can access the dashboard
+
+const CustomAPIError = require('../errors/custom-error')
 
 const login = async (req,res) => {
-    res.send('Fake Login/Register/Signup Route')
+    const {username, password} = req.body;
+
+    // mongoose validation
+    // Joi
+    // check in the controller
+
+    if(!username || !password) {
+        throw new CustomAPIError('Please provide email and password', 400)
+    }
+
+  
+    res.send('Fake Login/Register/Signup Route');
 }
 
 const dashboard = async (req,res) => {
